e965a217247be6d8c6c038d0f842a4cb3ab48f9f
amnotme <amnotme@icloud.com>
2018-05-22 12:45:14 -0700

    working on the second widget

--- a03-widgets/src/App.js
+++ a03-widgets/src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import { Clock } from './clock';
 import { Tabs } from './tabs';
+
+const Tab = [
+  {title: 'one', content: 'I am the first'},
+  {title: 'two', content: 'Second pane here'},
+  {title: 'three', content: 'Third pane here'}
+  ];
 class App extends Component {
+
   render() {
     return (
       <div className="App">
 		      <Clock />
-          <Tabs />
+          <Tabs tabs={ Tab } />
       </div>
     );
   }
